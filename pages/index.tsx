d0589a41423c2f5ff5030c466b4663d0b485081d
Joe <pyaephyokyaw.work@gmail.com>
2024-07-17 12:43:25 +0700

    chore: add env value

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { ethers } from "ethers";
 
 const abi = MyTokenContract.abi;
 
-const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
+const provider = new ethers.JsonRpcProvider(process.env.HARDHAT_RPC_URL);
 const contract = new ethers.Contract(
   "0x5FbDB2315678afecb367f032d93F642f64180aa3",
   abi,

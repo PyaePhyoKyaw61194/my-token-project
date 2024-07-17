e9f63de78ab05908b5ac1128cee85f0a9aec9f2d
Joe <pyaephyokyaw.work@gmail.com>
2024-07-17 13:36:19 +0700

    fix: using env values

--- utils/MyTokenContract.ts
+++ utils/MyTokenContract.ts
@@ -3,9 +3,11 @@ import { ethers } from "ethers";
 
 const abi = MyTokenContract.abi;
 
-const provider = new ethers.JsonRpcProvider(process.env.HARDHAT_RPC_URL);
+const provider = new ethers.JsonRpcProvider(
+  process.env.NEXT_PUBLIC_HARDHAT_RPC_URL
+);
 const contract = new ethers.Contract(
-  "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+  process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "",
   abi,
   provider
 );

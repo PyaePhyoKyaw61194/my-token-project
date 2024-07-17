4b5b7b33464e11bda9a83cd8b03e58722200ca29
Joe <pyaephyokyaw.work@gmail.com>
2024-07-17 17:42:49 +0700

    feat: adding sepolia

--- hooks/useContract.ts
+++ hooks/useContract.ts
@@ -5,9 +5,37 @@ const abi = MyTokenContract.abi;
 
 const useContract = () => {
   const [contract, setContract] = useState<ethers.Contract>();
+
   useEffect(() => {
     const initializeContract = async () => {
       if (typeof (window as any).ethereum !== "undefined") {
+        // for Browser wallet
+        /*   const provider = new ethers.BrowserProvider((window as any).ethereum);
+        const signer = await provider.getSigner();
+        const contractInstance = new ethers.Contract(
+          process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "",
+          abi,
+          signer
+        ); */
+
+        // For Sepolia
+        /*  const provider = new ethers.AlchemyProvider(
+          "sepolia",
+          process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
+        );
+
+        const wallet = new ethers.Wallet(
+          process.env.NEXT_PUBLIC_SEPOILA_PRIVATE_KEY || "",
+          provider
+        );
+
+        const contractInstance = new ethers.Contract(
+          process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "",
+          abi,
+          wallet
+        ); */
+
+        // for local development
         const provider = new ethers.JsonRpcProvider(
           process.env.NEXT_PUBLIC_HARDHAT_RPC_URL
         );
